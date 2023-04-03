e53dc8c4130d577eb0a230731546ad545935433e
AbdnHajar <abdennourihajar4@gmail.com>
2023-04-03 16:56:35 -0700

    file C

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _memcpy - function .
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
